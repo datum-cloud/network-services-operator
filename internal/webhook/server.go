1e695e184f4796042c8a3375e374c4b3ffa4097e
Joshua Reese <joshlreese@gmail.com>
2025-04-17 16:46:32 -0500

    URLDecode the cluster name provided to webhooks.

--- internal/webhook/server.go
+++ internal/webhook/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -18,7 +19,11 @@ var _ webhook.Server = &clusterAwareWebhookServer{}
 func (s *clusterAwareWebhookServer) Register(path string, hook http.Handler) {
 	if h, ok := hook.(*admission.Webhook); ok {
 		h.WithContextFunc = func(ctx context.Context, req *http.Request) context.Context {
-			return WithClusterName(ctx, req.PathValue("cluster_name"))
+			clusterName, err := url.QueryUnescape(req.PathValue("cluster_name"))
+			if err != nil {
+				return ctx
+			}
+			return WithClusterName(ctx, clusterName)
 		}
 	}
 

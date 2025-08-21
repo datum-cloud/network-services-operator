e8bb131f2df94c0e921ac433e587d989486a7581
Zach Smith <zachdsmith2015@gmail.com>
2025-08-21 16:18:25 -0700

    feat: use extra for project/cluster

--- internal/webhook/server.go
+++ internal/webhook/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"net/url"
 
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -22,13 +21,13 @@ var _ webhook.Server = &clusterAwareWebhookServer{}
 
 func (s *clusterAwareWebhookServer) Register(path string, hook http.Handler) {
 	if h, ok := hook.(*admission.Webhook); ok {
-		h.WithContextFunc = func(ctx context.Context, req *http.Request) context.Context {
-			clusterName, err := url.QueryUnescape(req.PathValue("cluster_name"))
-			if err != nil {
-				return ctx
+		orig := h.Handler
+		h.Handler = admission.HandlerFunc(func(ctx context.Context, req admission.Request) admission.Response {
+			if c := clusterFromExtra(req.UserInfo.Extra); c != "" {
+				ctx = mccontext.WithCluster(ctx, c)
 			}
-			return mccontext.WithCluster(ctx, clusterName)
-		}
+			return orig.Handle(ctx, req)
+		})
 	}
 
 	if s.discoveryMode != multiclusterproviders.ProviderSingle {
